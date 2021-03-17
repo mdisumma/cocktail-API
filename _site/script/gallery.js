94a1d02ca39c1cfb41008647ab7fb898764df6f5
Marco Di Summa <mdisumma@yahoo.com>
2021-03-17 03:31:28 +0000

    setting gallery api

--- _site/script/gallery.js
+++ _site/script/gallery.js
@@ -1,11 +1,26 @@
-url = 'https://the-cocktail-db.p.rapidapi.com/randomselection.php';
+const apiKey = "?rapidapi-key=a0ab4d64c9mshf661ca3514922aap1f9a12jsn3c719091a462"
+const Url = 'https://the-cocktail-db.p.rapidapi.com/randomselection.php';
+//https://example.p.rapidapi.com/?rapidapi-key=***************************
+const getData = async () => {
+    let { data } = await axios.get(Url + apiKey)
+    return data
+}
 
-//parse data in JSON format
-const createRequest = (url) => {
-    fetch(url)
-        .then((response) => ifError(response))//test for errors
-        .then((data) => {
-            const parsedData = (data);
-            console.log(parsedData);
-        });
-};
+
+//PARCE DATA
+document.addEventListener('DOMContentLoaded', async () => {
+    const results = await getData()
+    console.log(results);
+    for (const value of results.drinks) {
+
+        const printHTML = document.querySelector('#cocktailSelection');
+        printHTML.innerHTML += `
+        <a>
+        <figure>
+            <figcaption>${value.strDrink}</figcaption>
+            <img src="${value.strDrinkThumb}" width="320" alt="placeholder">
+        </figure>
+    </a>`;
+    }
+
+});
