bf46cba81ba51c5b934d5ab81e5a66e8a75ab8ea
Marco Di Summa <mdisumma@yahoo.com>
2021-03-27 14:41:24 +0000

    min

--- _site/script/target.js
+++ _site/script/target.js
@@ -97,10 +97,10 @@ const getCard = document.querySelector('#listByPopular').addEventListener('click
             frame.style.display = 'none';
         }
     }
-}, true)
+}, false)
 const hideCard = window.addEventListener('click', function (e) {
     const checkframe = document.querySelector('#frame')
     if (checkframe.style.display === 'grid') {
         frame.style.display = 'none';
     }
-}, true)
+}, false)
