7518454dd518d3306924aea4ac79241716f94e02
Marco Di Summa <mdisumma@yahoo.com>
2021-03-27 09:33:42 +0000

    test grid

--- _site/script/target.js
+++ _site/script/target.js
@@ -1,10 +1,10 @@
 frame.style.display = 'none'
 const getCard = document.querySelector('#listByPopular').addEventListener('click', function (e) {
     console.log(e)
-    // e.cancelBubble = true;
+    e.cancelBubble = true;
     const frame = document.querySelector('#frame')
     if (frame.style.display === 'none') {
-        frame.style.display = 'block'
+        frame.style.display = 'grid'
         if (e.target.localName === 'img') {
             let targetImg = e.path[1].children[0].innerHTML
             console.log(targetImg)
@@ -179,9 +179,9 @@ const getCard = document.querySelector('#listByPopular').addEventListener('click
         }
     }
 }, false)
-// const hideCard = window.addEventListener('click', function (e) {
-//     const checkframe = document.querySelector('#frame')
-//     if (checkframe.style.display === 'block') {
-//         frame.style.display = 'none';
-//     }
-// }, false)
+const hideCard = window.addEventListener('click', function (e) {
+    const checkframe = document.querySelector('#frame')
+    if (checkframe.style.display === 'grid') {
+        frame.style.display = 'none';
+    }
+}, false)
