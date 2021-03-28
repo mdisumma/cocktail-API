8bc87a2793f5e6cb3f336669157a70978a910350
Marco Di Summa <mdisumma@yahoo.com>
2021-03-28 12:54:02 +0100

    prettier

--- _site/script/gallery.js
+++ _site/script/gallery.js
@@ -1,28 +1,25 @@
-
-const apiKey = "?rapidapi-key=a0ab4d64c9mshf661ca3514922aap1f9a12jsn3c719091a462"
-const selectedUrl = 'https://the-cocktail-db.p.rapidapi.com/popular.php';
+const apiKey =
+	"?rapidapi-key=a0ab4d64c9mshf661ca3514922aap1f9a12jsn3c719091a462";
+const selectedUrl = "https://the-cocktail-db.p.rapidapi.com/popular.php";
 //https://example.p.rapidapi.com/?rapidapi-key=***************************
 
 //get data with axiom
 const getDataGallery = async () => {
-    let { data } = await axios.get(selectedUrl + apiKey)
-    return data
-}
+	let { data } = await axios.get(selectedUrl + apiKey);
+	return data;
+};
 
 // parse data
-document.addEventListener('DOMContentLoaded', async () => {
-    const results = await getDataGallery()
-    console.log(results);
+document.addEventListener("DOMContentLoaded", async () => {
+	const results = await getDataGallery();
+	console.log(results);
 
-
-    for (const value of results.drinks) {
-
-        const printHTML = document.querySelector('#listByPopular');
-        printHTML.innerHTML += `
+	for (const value of results.drinks) {
+		const printHTML = document.querySelector("#listByPopular");
+		printHTML.innerHTML += `
         <figure>
             <figcaption alt="${value.strDrink}">${value.strDrink} </figcaption>
             <img src="${value.strDrinkThumb}">
         </figure>`;
-    }
-
+	}
 });
